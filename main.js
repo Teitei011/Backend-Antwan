d17fa1fc803342755c32f1ad8689facc0af36255
Stefan Taiguara <45126099+Teitei011@users.noreply.github.com>
2023-02-04 17:07:33 -0300

    [ADIÇÃO] Criação da api

---
+++ main.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const ObjectId = mongodb.ObjectId;
+const app = express();
+const port = 3000;
+const uri = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/test?retryWrites=true&w=majority";
+
+const updateUser = async (userId, updatedUser) => {
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+  const db = client.db('exercise-diet-tracker');
+  const users = db.collection('users');
+  await users.updateOne({ _id: ObjectId(userId) }, { $set: updatedUser });
+  client.close();
+};
+
+const deleteUser = async (userId) => {
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+  const db = client.db('exercise-diet-tracker');
+  const users = db.collection('users');
+  await users.deleteOne({ _id: ObjectId(userId) });
+  client.close();
+};
+
+const insertUser = async (user) => {
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+  const db = client.db('exercise-diet-tracker');
+  const users = db.collection('users');
+  await users.insertOne(user);
+  client.close();
+};
+
+app.use(express.json());
+
+app.get('/users/:id', async (req, res) => {
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+  const db = client.db('exercise-diet-tracker');
+  const users = db.collection('users');
+  const user = await users.findOne({ _id: ObjectId(req.params.id) });
+  client.close();
+  res.json(user);
+});
+
+app.put('/users/:id', async (req, res) => {
+  const userId = req.params.id;
+  const updatedUser = req.body;
+  await updateUser(userId, updatedUser);
+  res.sendStatus(200);
+});
+
+app.delete('/users/:id', async (req, res) => {
+  const userId = req.params.id;
+  await deleteUser(userId);
+  res.sendStatus(200);
+});
+
+app.post('/users', async (req, res) => {
+  const user = req.body;
+  await insertUser(user);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`API listening at http://localhost:${port}`);
+});
