fd55fd89c49e2cba5e5e23d4e5f90e90c6d7cc73
Stefan Taiguara <45126099+Teitei011@users.noreply.github.com>
2023-02-06 09:21:39 -0300

    [BACKUP

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 require("dotenv").config();
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// const db = new MongoClient(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const db = new MongoClient(
+  "mongodb+srv://teitei:V6SkuL47wp1yH5ZG@cluster0.volrdxt.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  }
+);
+
 // mongoose.connect(process.env.DATABASE_URL, {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
-mongoose.connect("mongodb://localhost:27017/mydb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
