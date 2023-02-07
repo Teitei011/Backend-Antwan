584f8de069d31d329a3f383cefe5ac831225e9a0
Stefan Taiguara <45126099+Teitei011@users.noreply.github.com>
2023-02-07 09:37:05 -0300

    [ADIÇÃO] User está linkado com Exercise and Diet

--- test/controllers/user.js
+++ test/controllers/user.js
@@ -55,11 +55,114 @@ exports.updateUser = async (req, res) => {
   }
 };
 
+
+
+
 exports.deleteUser = async (req, res) => {
   try {
     const user = await User.findByIdAndRemove(req.params.id);
     if (!user) return res.status(404).json({ message: 'User not found' });
     res.json({ message: 'User deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+
+exports.addExercise = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    user.exercises.push({
+      title: req.body.title,
+      subtitle: req.body.subtitle,
+      link: req.body.link
+    });
+
+    const updatedUser = await user.save();
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// edit Exercise
+exports.updateUserExercise = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: { exercise: req.body.exercise } },
+      { new: true }
+    );
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// delete Exercise
+exports.deleteUserExercise = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    const index = user.exercises.findIndex(d => d._id.toString() === req.params.dietId);
+    if (index === -1) return res.status(404).json({ message: 'Diet not found' });
+
+    user.exercises.splice(index, 1);
+    await user.save();
+    res.json({ message: 'Exercise deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+// DIET
+
+exports.addDietToUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    user.diet.push(req.body.diet);
+    await user.save();
+
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// edit Diet
+exports.updateUserDiet = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: { diet: req.body.diet } },
+      { new: true }
+    );
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+// delete Diet
+exports.deleteUserDiet = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    const index = user.diet.findIndex(d => d._id.toString() === req.params.dietId);
+    if (index === -1) return res.status(404).json({ message: 'Diet not found' });
+
+    user.diet.splice(index, 1);
+    await user.save();
+    res.json({ message: 'Diet deleted' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
