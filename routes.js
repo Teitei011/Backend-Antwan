267a7fb27ef92c51eb50e3329f64e22cb37ff437
Teitei011 <stefantleal14@gmail.com>
2023-02-08 12:40:03 -0300

    [LIMPEZA] Remoção de tudo que não está ajudando no código

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user');
-const dietController = require('../controllers/diet');
-const exerciseController = require('../controllers/exercise');
+const userController = require('./controllers/user');
+const dietController = require('./controllers/diet');
+const exerciseController = require('./controllers/exercise');
 
 // User routes
 router.get('/users', userController.getUsers);
@@ -10,6 +10,20 @@ router.get('/user/:id', userController.getUser);
 router.post('/user', userController.createUser);
 router.patch('/user/:id', userController.updateUser);
 router.delete('/user/:id', userController.deleteUser);
+
+// user exercises routes 
+
+// router.get('/user/:id/exercise', userController.getUserDiets);
+router.post('/user/:id/exercise', userController.addUserExercise);
+router.patch('/user/:id/exercise/:exerciseID', userController.updateUserExercise);
+router.delete('/user/:id/exercise/:exerciseID', userController.deleteUserExercise);
+
+
+// user diets routes
+// router.get('/user/:id/diets', userController.getUserDiets);
+router.post('/user/:id/diet', userController.addUserDiet);
+router.patch('/user/:id/diet/:dietId', userController.updateUserDiet);
+router.delete('/user/:id/diet/:dietId', userController.deleteUserDiet);
 
 // Diet routes
 router.get('/diets', dietController.getDiets);
