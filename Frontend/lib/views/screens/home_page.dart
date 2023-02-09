a1a2ec14f45f6b9dc443826996d8e350fdd89211
Teitei011 <stefantleal14@gmail.com>
2023-02-09 18:01:10 -0300

    [BUG FIXED] Null safety fixed

--- Frontend/lib/views/screens/home_page.dart
+++ Frontend/lib/views/screens/home_page.dart
@@ -1,24 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:hungry/models/core/recipe.dart';
-import 'package:hungry/models/helper/recipe_helper.dart';
-// import 'package:hungry/views/screens/delicious_today_page.dart';
-// import 'package:hungry/views/screens/newly_posted_page.dart';
-import 'package:hungry/views/screens/profile_page.dart';
-// import 'package:hungry/views/screens/search_page.dart';
-// import 'package:hungry/views/utils/AppColor.dart';
 import 'package:hungry/views/widgets/custom_app_bar.dart';
-// import 'package:hungry/views/widgets/dummy_search_bar.dart';
-// import 'package:hungry/views/widgets/featured_recipe_card.dart';
-// import 'package:hungry/views/widgets/recipe_tile.dart';
-// import 'package:hungry/views/widgets/recommendation_recipe_card.dart';
-
+import 'package:hungry/models/helper/recipe_helper.dart';
+import 'package:hungry/views/screens/profile_page.dart';
+import 'package:hungry/models/core/recipe.dart';
 import '../widgets/texto_motivacional.dart';
+import 'package:flutter/material.dart';
 import '../widgets/treinos.dart';
 
 class HomePage extends StatelessWidget {
   final List<Recipe> featuredRecipe = RecipeHelper.featuredRecipe;
   final List<Recipe> recommendationRecipe = RecipeHelper.recommendationRecipe;
   final List<Recipe> newlyPostedRecipe = RecipeHelper.newlyPostedRecipe;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,19 +33,30 @@ class HomePage extends StatelessWidget {
             child: TextoMotivacional(),
           ),
           Container(
-              height: 1750,
-              child: Treinos(exercises: {
-                'exercises': {
-                  {'title': 'Squats', 'subtitle': '3 sets of 10 reps'},
-                  {'title': 'Deadlifts', 'subtitle': '3 sets of 8 reps'},
-                  {'title': 'Push ups', 'subtitle': '3 sets of 10 reps'},
-                }.toSet(),
-              }, diets: {
-                'diets': {
-                  {'title': 'Arroz', 'subtitle': '300g'},
-                  {'title': 'Frango', 'subtitle': '200g'},
-                }.toSet(),
-              })),
+            height: 1750,
+            child: Treinos(
+              exercises: {
+                'A': [
+                  {'title': 'Exercise 1', 'subtitle': '3 sets of 10 reps'},
+                  {'title': 'Exercise 2', 'subtitle': '3 sets of 8 reps'},
+                ],
+                'B': [
+                  {'title': 'Exercise 3', 'subtitle': '3 sets of 10 reps'},
+                  {'title': 'Exercise 4', 'subtitle': '3 sets of 8 reps'},
+                ],
+              },
+              diets: {
+                'breakfast': [
+                  {'title': 'Food 1', 'subtitle': '300g'},
+                  {'title': 'Food 2', 'subtitle': '200g'},
+                ],
+                'postWorkout': [
+                  {'title': 'Food 3', 'subtitle': '100g'},
+                  {'title': 'Food 4', 'subtitle': '150g'},
+                ],
+              },
+            ),
+          ),
         ],
       ),
     );
--- Frontend/lib/views/widgets/treinos.dart
+++ Frontend/lib/views/widgets/treinos.dart
@@ -1,12 +1,16 @@
-// exercises.dart
 import 'package:flutter/material.dart';
 
-class Treinos extends StatelessWidget {
-  final Map<String, Set<Map<String, String>>> exercises;
-  final Map<String, Set<Map<String, String>>> diets;
+class Treinos extends StatefulWidget {
+  final Map<String, List<Map<String, String>>> exercises;
+  final Map<String, List<Map<String, String>>> diets;
 
   Treinos({required this.exercises, required this.diets});
 
+  @override
+  _TreinosState createState() => _TreinosState();
+}
+
+class _TreinosState extends State<Treinos> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,36 +21,72 @@ class Treinos extends StatelessWidget {
               'Exercícios',
               textAlign: TextAlign.center,
             ),
-            children: exercises['exercises']!.map((exercise) {
-              return ListTile(
-                title: Text(
-                  exercise['title']!,
-                  textAlign: TextAlign.center,
-                ),
-                subtitle: Text(
-                  exercise['subtitle']!,
-                  textAlign: TextAlign.center,
-                ),
-              );
-            }).toList(),
+            children: widget.exercises['A'] != null
+                ? [
+                    ...['A', 'B', 'C', 'D', 'E'].map((exerciseType) {
+                      return Column(
+                        children: <Widget>[
+                          ...?widget.exercises[exerciseType]?.map((exercise) {
+                            return ListTile(
+                              title: Text(
+                                exercise['title'] ?? '',
+                                textAlign: TextAlign.center,
+                              ),
+                              subtitle: Text(
+                                exercise['subtitle'] ?? '',
+                                textAlign: TextAlign.center,
+                              ),
+                            );
+                          }).toList(),
+                        ],
+                      );
+                    }).toList()
+                  ]
+                : [
+                    Center(
+                      child: Text('Não há exercícios'),
+                    ),
+                  ],
           ),
           ExpansionTile(
             title: Text(
               'Comida',
               textAlign: TextAlign.center,
             ),
-            children: diets['diets']!.map((diet) {
-              return ListTile(
-                title: Text(
-                  diet['title']!,
-                  textAlign: TextAlign.center,
-                ),
-                subtitle: Text(
-                  diet['subtitle']!,
-                  textAlign: TextAlign.center,
-                ),
-              );
-            }).toList(),
+            children: widget.diets['breakfast'] != null
+                ? [
+                    ...[
+                      'breakfast',
+                      'postWorkout',
+                      'morningSnack',
+                      'lunch',
+                      'afternoonSnack',
+                      'dinner',
+                      'nightSnack'
+                    ].map((dietType) {
+                      return Column(
+                        children: <Widget>[
+                          ...?widget.diets[dietType]?.map((diet) {
+                            return ListTile(
+                              title: Text(
+                                diet['title'] ?? '',
+                                textAlign: TextAlign.center,
+                              ),
+                              subtitle: Text(
+                                diet['subtitle'] ?? '',
+                                textAlign: TextAlign.center,
+                              ),
+                            );
+                          }).toList(),
+                        ],
+                      );
+                    }).toList()
+                  ]
+                : [
+                    Center(
+                      child: Text('Não há dietas'),
+                    ),
+                  ],
           ),
         ],
       ),
