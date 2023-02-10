95b74b20d0c9f253067ca026588225a00f6d87f1
Teitei011 <stefantleal14@gmail.com>
2023-02-10 10:00:18 -0300

    [SUBSITITUIÇÃO] Substituindo o antigo programa pelo novo

--- Frontend/test/widget_test.dart
+++ Frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hungry/main.dart';
+import 'package:best_flutter_ui_templates/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
