9c1b2228654afb9b1c39b978604e08ec74fd7e6b
Teitei011 <stefantleal14@gmail.com>
2023-02-13 08:54:50 -0300

    [BACKUP]

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
       systemNavigationBarIconBrightness: Brightness.dark,
     ));
     return MaterialApp(
-      title: 'Flutter UI',
+      title: 'Bora Treinar?',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
