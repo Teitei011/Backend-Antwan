35ec2030f5807c91354252c4f51b04c59af3d300
Teitei011 <stefantleal14@gmail.com>
2023-02-15 16:29:54 -0300

    [CORREÇÃO DE BUG] Agora dá pra clicar no botão inteiro

--- Frontend/lib/utils/api/exercise_api.dart
+++
@@ -1,9 +0,0 @@
-import "package:http";
-
-class ExerciseApi {
-  static Future getExercises() async {
-    var url = "https://wger.de/api/v2/exercise/?format=json";
-    var response = await http.get(url);
-    return json.decode(response.body);
-  }
-}
