dd95bbb63ce41a07949767237735f72c1c6332d9
Teitei011 <stefantleal14@gmail.com>
2023-02-24 16:20:29 -0300

    [ADIÇÃO] Icones estão nos seus lugares

--- Frontend/lib/screens/diet_screen.dart
+++ Frontend/lib/screens/diet_screen.dart
@@ -29,6 +29,92 @@ class _DietScreenState extends State<DietScreen> {
                       color: AppTheme.darkText,
                       fontSize: 30,
                       fontWeight: FontWeight.bold)),
+
+              Container(
+                height: 1500,
+                margin: const EdgeInsets.only(top: 20),
+                child: ListView(children: [
+                  Card(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/fitness_app/breakfast.png',
+                          width: 100,
+                          height: 100,
+                        ),
+                        Text('Café da manhã'),
+                        Text('300 kcal'),
+                        Column(
+                          children: [
+                            Text('Ovo'),
+                            Text('Pão'),
+                            Text('Leite'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Card(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/fitness_app/lunch.png',
+                          width: 100,
+                          height: 100,
+                        ),
+                        Text('Almoço'),
+                        Text('500 kcal'),
+                        Column(
+                          children: [
+                            Text('Arroz'),
+                            Text('Feijão'),
+                            Text('Carne'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Card(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/fitness_app/dinner.png',
+                          width: 100,
+                          height: 100,
+                        ),
+                        Text('Jantar'),
+                        Text('400 kcal'),
+                        Column(
+                          children: [
+                            Text('Macarrão'),
+                            Text('Carne'),
+                            Text('Salada'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Card(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/fitness_app/snack.png',
+                          width: 100,
+                          height: 100,
+                        ),
+                        Text('Lanche'),
+                        Text('200 kcal'),
+                        Column(
+                          children: [
+                            Text('Biscoito'),
+                            Text('Leite'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ]),
+              )
             ]),
           ),
         ),
