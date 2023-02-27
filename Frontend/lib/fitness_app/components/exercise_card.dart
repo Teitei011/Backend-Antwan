d134c2787c1cf184c78a74c6329027e10ff883b5
Teitei011 <stefantleal14@gmail.com>
2023-02-27 12:02:09 -0300

    [CORREÇÃO DE BUG] Agora os exercícios estão ocupando a tela inteira

--- Frontend/lib/fitness_app/components/exercise_card.dart
+++ Frontend/lib/fitness_app/components/exercise_card.dart
@@ -1,66 +1,63 @@
 import "package:flutter/material.dart";
+import "package:naturalteam/app_theme.dart";
 
 import '../../screens/training/body_exercise_screen.dart';
 import "../models/exercise.dart";
 
-class ExerciseCard extends StatelessWidget {
-  const ExerciseCard({
-    super.key,
-    required this.list,
-    required this.animation,
-    required this.animationController,
-    required this.index,
-  });
+List list = [
+  [
+    'Costas',
+    'Ombro',
+    'Biceps, Triceps',
+    'Peito',
+    'Perna',
+  ],
+  [
+    'assets/images/back.png',
+    'assets/images/back2.png',
+    'assets/images/biceps.png',
+    'assets/images/chest.png',
+    'assets/images/leg.png'
+  ],
+  ["A", "B", "C", "D", "E"]
+];
 
-  final List list;
-  final index;
-  final Animation<double> animation;
-  final AnimationController? animationController;
+class ExerciseCard extends StatelessWidget {
+  const ExerciseCard({Key? key, required this.index}) : super(key: key);
+
+  final int index;
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: InkWell(
-        onTap: () {
-          print("Pressed: " + list[2][index]);
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => BodyExerciseScreen(
-                imagePath: list[1][index],
-                titleTxt: list[0][index],
-                exercises: exerciciosTemplate,
-                exerciseType: list[2][index],
-              ),
+    return InkWell(
+      onTap: () => {print(list[2][index])},
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(50),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            color: AppTheme.nearlyWhite,
+            child: Column(
+              children: [
+                Text(
+                  list[0][index],
+                  style: TextStyle(
+                    color: AppTheme.darkText,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Image.asset(
+                  list[1][index],
+                  width: 175,
+                  height: 175,
+                ),
+              ],
             ),
-          );
-        },
-        child:
-            ListView(physics: const NeverScrollableScrollPhysics(), children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                list[0][index],
-                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-
-              Image.asset(
-                list[1][index],
-                height: 150,
-              ),
-              // AreaView(
-              //   imagepath: list[1][index],
-              //   animation: animation,
-              //   animationController: animationController!,
-              // ),
-            ],
           ),
-        ]),
+        ),
       ),
     );
   }
